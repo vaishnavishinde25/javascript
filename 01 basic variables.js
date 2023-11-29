b3ef7e7772a5d0dd087263d68991db47c1173919
Vaishnavi Shinde <vaishnavishinde2511@gmail.com>
2023-11-29 22:58:27 +0530

    .
    dataTypes

---
+++ 01 basic variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "vaishnavi@google.com"
+var accountPassword = "12345"
+accountCity = "karad"
+let accountState;
+
+accountEmail = "vaishnavishinde2511@gmail.com"
+accountPassword = "1234567789"
+accountCity = "pune"
+
+// 2 accountID not allowed
+// prefer not use var
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountState])
---
+++ 02 datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js node as newer version
+
+// alert(3 + 3) // we are using node js, not browser
+
+console.log(3
+
+        +
+        3) // code readability shoud be high 
+
+console.log("vaishnavi")
+
+let name = "vaishnavi"
+let age = 22
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+//bigint (use to only large numbers )
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined =>
+// symbol => uinque
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null); // object
